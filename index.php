231d4eddf8b8fb6ede0fc51c6a6570dff086ba0e
Killianstr <stricot.killian@gmail.com>
2018-11-05 16:07:58 +0100

    Création de l'ihm de base : 4 boutons et 2 inputs text pour 2 moteurs

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+ <head>
+   <title>Mon premier site</title>
+ </head>
+ <body>
+  <div id="titre1" class="titre" style="border: solid;">
+  <h1>Mon site web</h1></div>
+    <p>Salut le BTS SN IR</p>
+    <?php
+         echo"Nous sommes ajd ".date("l").".";
+         phpinfo();
+    ?>  
+    <a href="www.google.fr">le lien </a> 
+ </body>
+
+<html>
+ <head>
+  <meta charset="utf-8" />
+  <title>CommandeRobot</title>
+
+  <body>
+   <div id="titre1" class="titre1" style="border thick solid">
+    <input type="button"/>
+    <input type="text"/>
+    <input type="button"/>
+    <input type="button"/>
+   </div>
+  </body>
+</html>
